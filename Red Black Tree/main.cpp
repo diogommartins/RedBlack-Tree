a610315b544a9246fc8360641ac935f8b0c0c556
diogommartins <diogo.martins@unirio.br>
2015-10-19 13:39:47 -0200

    Initial Commit

---
+++ Red Black Tree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Red Black Tree
+//
+//  Created by Diogo  Martins on 10/19/15.
+//  Copyright © 2015 Diogo Magalhães Martins. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
